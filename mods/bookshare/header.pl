88e4a6a0453229718fa540c5f4a08278f36c2a1f
chris <chandwer@student.fitchburgstate.edu>
2013-04-26 02:10:36 -0400

    EC Diagram

--- mods/bookshare/header.pl
+++ mods/bookshare/header.pl
@@ -2,6 +2,4 @@
 
 $title = "FSU BookShare";
 if($ARGV[0]) { $title.=" - $ARGV[0]"; }
-print <<HEADER;
-<div class=banner><a href=/><p class=title>$title</p></a></div>
-HEADER
+print "<div class=banner><a href=/><p class=title>$title</p></a></div>";
